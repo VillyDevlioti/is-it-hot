beea91a31d591afef5937159ca9b731528111018
Villy Devlioti <vadevlio@gmail.com>
2020-07-19 00:07:42 -0400

    Updates

---
+++ precache-manifest.ca6f443be2d66015442c6d1182052d80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4f8eeceb148ff0c18d5531fc1a5224",
+    "url": "/is-it-hot/index.html"
+  },
+  {
+    "revision": "ed0ec50f9a82da17ee46",
+    "url": "/is-it-hot/static/css/2.c47c3995.chunk.css"
+  },
+  {
+    "revision": "8f7e8bd1fbc96edb6ad4",
+    "url": "/is-it-hot/static/css/main.ce205e60.chunk.css"
+  },
+  {
+    "revision": "ed0ec50f9a82da17ee46",
+    "url": "/is-it-hot/static/js/2.2d44960f.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/is-it-hot/static/js/2.2d44960f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7e8bd1fbc96edb6ad4",
+    "url": "/is-it-hot/static/js/main.61b759a1.chunk.js"
+  },
+  {
+    "revision": "c14d6484b944ab63d9da",
+    "url": "/is-it-hot/static/js/runtime-main.d3557eeb.js"
+  }
+]);
